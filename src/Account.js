63365c1ff8e025e68b7fe481c91082f15f04a31c
Scott <scrosehart@gmail.com>
2018-03-09 12:33:06 -0800

    working on transfer

--- src/Account.js
+++ src/Account.js
@@ -1,43 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      balance: 0
-    }
-
-    this.handleDepositClick = this.handleDepositClick.bind(this)
-  }
-
-  handleDepositClick(e) {
-    e.preventDefault();
-    if (isNaN(this.refs.amount.value)) {
-      console.log("Not a number");
-    }
-    else {
-      let amount = +this.refs.amount.value;
-      let newBalance = this.state.balance + amount;
-      this.setState({
-        balance: newBalance
-      })
-      this.refs.amount.value = '';
-    }
-  }
 
   render() {
+
+    const total = parseInt(this.props.balance) + parseInt(this.props.transfer)
+
     let balanceClass = 'balance';
-    if (this.state.balance === 0) {
+    if (total === 0) {
       balanceClass += ' zero';
     }
 
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className={balanceClass}>${this.state.balance}</div>
+        <div className={balanceClass}>${total}</div>
         <input type="text" placeholder="enter an amount" ref="amount" />
-        <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Deposit" onClick={(e) => this.props.handleDepositClick(e, this.refs.amount.value, this.props.name)} />
+        <input type="button" value="Withdraw" onClick={(e) => this.props.handleWithdrawClick(e, this.refs.amount.value, this.props.name)} />
+        <input type="button" value="Transfer" onClick={() => this.handleTransferClick(this.refs.amount.value, this.props.name)} />
       </div>
     )
   }
--- src/App.js
+++ src/App.js
@@ -4,6 +4,66 @@ import logo from './ga.png';
 import Account from './Account';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checking: 0,
+      savings: 0,
+      transfer: 0
+    }
+    this.handleDepositClick = this.handleDepositClick.bind(this)
+    this.handleWithdrawClick = this.handleWithdrawClick.bind(this)
+  }
+
+  handleDepositClick(e, amount, name) {
+    e.preventDefault();
+    if (isNaN(amount)) {
+      console.log("Not a number");
+    }
+    else {
+      let amount = +amount;
+      let newBalance = 0;
+      if (name === 'Checking') {
+        newBalance = +this.state.checking + amount
+        this.setState({
+          checking: newBalance
+        })
+      } else {
+        newBalance = +this.state.savings + amount
+        this.setState({
+          savings: newBalance
+        })
+      }
+    }
+  }
+
+  handleWithdrawClick(e, amount, name) {
+    e.preventDefault();
+    if (isNaN(amount)) {
+      console.log("Not a number");
+    }
+    else {
+      let amount = +amount;
+      let newBalance = 0;
+      if (name === 'Checking') {
+        newBalance = +this.state.checking - amount
+        this.setState({
+          checking: newBalance
+        })
+      } else {
+        newBalance = +this.state.savings - amount
+        this.setState({
+          savings: newBalance
+        })
+      }
+    }
+  }
+
+  handleTransferClick() {
+    const newTransferState = {}
+    name === 'Checking' ? name = 'Savings' : name = 'Checking'
+  }
+
   render() {
     return (
       <div id="content">
@@ -11,7 +71,20 @@ class App extends Component {
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account name="Checking" />
+        <Account name="Checking"
+          balance={this.state.checking}
+          transfer={this.state.transfer}
+          handleDepositClick={this.handleDepositClick}
+          handleWithdrawClick={this.handleWithdrawClick}
+          handleTransferClick={this.handleTransferClick}
+        />
+        <Account name="Savings"
+          balance={this.state.savings}
+          transfer={this.state.transfer}
+          handleDepositClick={this.handleDepositClick}
+          handleWithdrawClick={this.handleWithdrawClick}
+          handleTransferClick={this.handleTransferClick}
+        />
         <div className="clear"></div>
       </div>
     );
